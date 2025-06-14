64c120c9265443c3c6e699a6cb7d7dea06b7b8d8
Bikash <60249539+dashbikash@users.noreply.github.com>
2025-06-14 09:40:05 +0000

    dynamodb

--- tf-aws-cts-ccl/dynamodb.tf
+++ tf-aws-cts-ccl/dynamodb.tf
@@ -1,18 +1,26 @@
 resource "aws_dynamodb_table" "dynamo_bikash_tbl" {
-    name           = "dynamo_bikash_tbl"
-    billing_mode   = "PAY_PER_REQUEST"
+  name         = "dynamo_bikash_tbl"
+  billing_mode = "PAY_PER_REQUEST"
 
-    hash_key       = "id"
+  hash_key = "id"
+  range_key = "dob"
 
-    attribute {
-        name = "id"
-        type = "S"
-        
-    }
+  attribute {
+    name = "id"
+    type = "S"
+  }
+  attribute {
+    name = "dob"
+    type = "N"
+  }
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
 
-    tags = {
-        Environment = "dev"
-        Name        = "dynamo_bikash_tbl"
-        Author      = "Bikash"
-    }
+  tags = {
+    Environment = "dev"
+    Name        = "dynamo_bikash_tbl"
+    Author      = "Bikash"
+  }
 }
